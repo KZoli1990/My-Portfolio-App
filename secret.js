ea7c62854b3e2ebd7eb942dbb08e27a3230b95f3
Keresztenyi Zoltan <Kerzocarbon@gmail.com>
2023-09-23 14:20:44 +0200

    Mivel em sikerült a titkosított dolgokat .env fájlba elmenteni ezért csak így tudtam megoldani

---
+++ secret.js
@@ -0,0 +1,4 @@
+export const SERVICE_ID = 'service_5hfezhi';
+export const TEMPLATE_ID = 'template_adn6wfj';
+export const PUBLIC_KEY = 'W6FpScHT7M2Xl_3Pv';
+/* for some reason the .env file system didn't work emailjs.sendForm(procces.env.SERVICE_ID, procces.env.TEMPLATE_ID, e.target, procces.env.PUBLIC_KEY); */
